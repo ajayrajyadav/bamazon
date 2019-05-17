3074d15e8f4c96abbcee34944310f3d66779453e
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-05-17 16:25:15 -0700

    adding department is being worked on.

--- bamazonExecutive.js
+++ bamazonExecutive.js
@@ -9,18 +9,62 @@ const mainMenu = require('./mainMenu');
 
 function ViewProductSales(callback) {
     console.log("in viewProduct function");
+    let query = "SELECT * FROM totalprofits";
+    dbQueries.doQuery(query, function (error, data) {
+        printTable(data, function () {
+            mainMenu.showMainMenu(executiveOptions, callback);
+        });
+    })
+}
+
+function printTable(result, callback) {
+    console.log(result);
+    
+    var displayTable = new table({
+        head: ['Department ID', 'Department Name', 'Overhead Cost', 'Total Sales', 'Total Profit'],
+        style: {
+            head: ["red"],
+            compact: false,
+            colAligns: ["center"],
+        }
+    });
+
+    for (let i = 0; i < result.length; i++) {
+        displayTable.push(
+            [result[i].DepartmentID, result[i].DepartmentName, result[i].OverHeadCosts, result[i].TotalSales, result[i].TotalProfit]
+        )
+    }
+    
+    console.log("\n")
+    console.log(displayTable.toString());
+    console.log("\n");
     callback();
-    // bamazonCustomer.displayInventory(function () {
-    //     mainMenu.showMainMenu(managerOptions, callback);
-    // });
 }
 
 function createDepartment(callback) {
-    console.log("in createDepartment function: ");
-    callback();
-    // bamazonCustomer.displayInventory(function () {
-    //     mainMenu.showMainMenu(managerOptions, callback);
-    // });
+    bamazonCustomer.displayInventory(function () {//need to take this out.
+        addCustomerHelper(callback);
+    });
+}
+
+function addCustomerHelper(callback){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deptName',
+            message: 'Please enter the nae of the new department you would like to add.',
+        },
+        {
+            type: 'input',
+            name: 'overheadCost',
+            message: 'What are the overhead costs for theis department?',
+            filter: Number
+        }
+    ])
+    .then(function (answer){
+        let queryInsert = "INSERT INTO departments(DepartmentName, OverHeadCosts, TotalSales) VALUES (\"" + answer.deptName + "\"," + answer.overheadCost + ", 0.0)";
+        console.log(queryInsert) 
+    })
 }
 
 const executiveOptions = {
@@ -33,7 +77,7 @@ function backToMainMenu(Callback){
     Callback();
 }
 function printExecutiveOptions(callback){
-    mainMenu.showMainMenu(executiveOptions, callback);
+    mainMenu.showMainMenu(executiveOptions, "Executive Menu", callback);
 }
 
 module.exports = {
--- mainMenu.js
+++ mainMenu.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 
 function showMainMenu(options, callback) {
+  console.log("\n\n");
   const userOptions = Object.keys(options);
 
   inquirer.prompt([
